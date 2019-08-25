685fe40b0b3012a5b7045a34a21505c4e78e08a8
Autumn Ragland <46455498+autumn-ragland@users.noreply.github.com>
2019-08-25 15:27:57 -0500

    home screen, login page, list of invoices, and view invoices

--- app/src/main/java/com/example/browncarinvoice/Main3Activity.java
+++ app/src/main/java/com/example/browncarinvoice/Main3Activity.java
@@ -2,7 +2,9 @@ package com.example.browncarinvoice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+
+    public void goToInvoiceInfo(View view) {
+        Intent intent = new Intent(this,Main4Activity.class);
+        startActivity(intent);
+    }
+
+    public void goToNewInvoice(View view) {
+        Intent intent = new Intent(this, Main5Activity.class);
+        startActivity(intent);
     }
 }
